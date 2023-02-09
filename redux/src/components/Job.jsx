d378c1cb8d07df82bd64e720a41ae138871c6686
MantasPetrosiusI <mantas.petrosius@outlook.com>
2023-02-09 17:31:02 +0000

    1

--- redux/src/components/Job.jsx
+++ redux/src/components/Job.jsx
@@ -1,6 +1,6 @@
 import { Row, Col, Button} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
 
 
 const Job = ({ data }) => {
